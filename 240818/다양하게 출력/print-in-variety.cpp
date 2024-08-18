2937e4c500450e44b21d6d48eef11f0a851c2411
Kim Chaewon <101501925+Dim-chae@users.noreply.github.com>
2024-08-18 18:03:28 +0900

    Title: 다양하게 출력, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240818/다양하게 출력/print-in-variety.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << fixed;
+    cout.precision(10);
+    cout << "Total days in Year" << '\n' << 365 << '\n' << "Circumference rate" << '\n' << 3.1415926535;
+    return 0;
+}
